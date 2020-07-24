6a06e553ffe412fdf1b9fbc33e390058230863c4
yuanzhu <yzsylinder@163.com>
2020-07-24 23:20:49 +0800

    [parkingBoy] [caps] feature: pass should_return_null_when_parking_given_car_and_remain_zero_position

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -5,16 +5,28 @@ import java.util.LinkedList;
 import java.util.Map;
 
 public class ParkingBoy {
+    private int remainPosition;
     private Map<Car, Ticket> carToTicket;
     private Map<Ticket, Car> ticketToCar;
 
     public ParkingBoy() {
+        remainPosition = 10;
         this.carToTicket = new HashMap<>();
         this.ticketToCar = new HashMap<>();
     }
 
+    public void setRemainPosition(int remainPosition) {
+        this.remainPosition = remainPosition;
+    }
+
+    public int getRemainPosition() {
+        return remainPosition;
+    }
 
     public Ticket park(Car car) {
+        if (getRemainPosition() == 0) {
+            return null;
+        }
         if (!carToTicket.containsKey(car)) {
             Ticket ticket = new Ticket();
             carToTicket.put(car, ticket);
