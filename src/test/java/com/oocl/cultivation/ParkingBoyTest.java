3d27ecd1cc62d2984314ed665965ce210b7bc2ad
caps <yzsylinder@163.com>
2020-07-24 16:35:29 +0800

    [parkingBoy] [caps] test: rewrite should_return_car_when_parking_given_ticket

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -7,6 +7,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingBoyTest {
 
+    /**
+     * given: a car, parking boy;
+     * when: parkingboy parking car
+     * then: return a ticket
+     */
+
     @Test
     void should_return_ticket_when_parking_given_car() {
         Car car = new Car();
@@ -17,17 +23,54 @@ class ParkingBoyTest {
 
 
     /**
-     * given: ticket
-     * when: parking
-     * then: return a car
+     * given: a ticket
+     * when: parking boy parking car
+     * then: return a correspond car
      */
 
     @Test
     public void should_return_car_when_parking_given_ticket() {
-        Ticket ticket = new Ticket();
+        Car car = new Car();
         ParkingBoy parkingBoy = new ParkingBoy();
-        Car car = parkingBoy.fetchCar(ticket);
-        Assertions.assertNotNull(car);
+        Ticket ticket = parkingBoy.park(car);
+        Car returnCar = parkingBoy.fetchCar(ticket);
+        Assertions.assertEquals(car.getId(), returnCar.getId());
     }
+
+    /**
+     * given:  2 cars
+     * when: parking boy parking car
+     * then: return 2 ticket
+     */
+
+
+    /**
+     * given: a ticket
+     * when: parking boy fetch car by ticket
+     * then: return a correspond car
+     */
+
+
+    /**
+     * given: a wrong ticket
+     * when: parking boy fetch car by ticket
+     * then: return null and a warning message
+     */
+
+
+    /**
+     * given: a ticket already used
+     * when: parking boy fetch car
+     * then: return null and a warning message
+     */
+
+
+    /**
+     * given: no position, a car
+     * when: parking boy parking car
+     * then: return null and a sorry message.
+     */
+
+
 
 }
