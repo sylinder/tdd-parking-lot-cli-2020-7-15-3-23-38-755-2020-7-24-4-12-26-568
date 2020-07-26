e4da8e495d20d75e2d12beb3611f0b246bba2dc1
yuanzhu <yzsylinder@163.com>
2020-07-26 11:10:33 +0800

    [parkingBoy] [caps] feature: pass should_return_decrease_position_with_larger_rate_when_parking_given_two_diff_available_rate

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -1,10 +1,60 @@
 package com.oocl.cultivation;
 
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class SuperSmartParkingBoy extends ParkingBoy{
 
     public SuperSmartParkingBoy(LinkedList<ParkingLot> parkingLots) {
         super(parkingLots);
+    }
+
+    @Override
+    public Ticket park(Car car) {
+        if (car == null) {
+            return null;
+        }
+
+        int totalRemainPosition = getTotalRemainPosition();
+        if (totalRemainPosition == 0) {
+            return  null;
+        }
+
+        Map<Ticket, Car> ticketToCar = super.getTicketToCar();
+        LinkedList<ParkingLot> parkingLots = super.getParkingLots();
+        if (!ticketToCar.containsValue(car)) {
+            Ticket ticket = new Ticket();
+            ticketToCar.put(ticket, car);
+            int position = getParkingLotWithMostAvailablePosition(parkingLots);
+            parkingLots.get(position).decreasePostion();
+            return ticket;
+        }
+        return null;
+    }
+
+//    public int getParkingLotWithMostEmptyPosition(LinkedList<ParkingLot> parkingLots) {
+//        int position = -1;
+//        int maxEmptyPosition = 0;
+//        for (int index = 0; index < parkingLots.size(); index++) {
+//            if (parkingLots.get(index).getRemainPosition() > maxEmptyPosition) {
+//                maxEmptyPosition = parkingLots.get(index).getRemainPosition();
+//                position = index;
+//            }
+//        }
+//        return position;
+//    }
+
+    public int getParkingLotWithMostAvailablePosition(LinkedList<ParkingLot> parkingLots) {
+        int position = -1;
+        double mostAvailable = 0.0;
+        for (int index = 0; index < parkingLots.size(); index++) {
+            double availableRate = (double)parkingLots.get(index).getRemainPosition() / parkingLots.get(index).getTotalPosition();
+            if (availableRate > mostAvailable) {
+                mostAvailable = availableRate;
+                position = index;
+            }
+        }
+        return position;
     }
 }
